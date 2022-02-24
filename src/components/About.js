d751840378ba34032a42153a55605515912506b9
Grant Royseth <groyseth@gmail.com>
2022-02-24 17:20:14 -0600

    about

--- src/components/About.js
+++ src/components/About.js
@@ -18,9 +18,7 @@ export default function About() {
           <div className="container moving">
             <h1 className=" ">Hello im Grant</h1>
             <h1 >About Me:</h1>
-            <p >I am a student at the Minnesota State University becoming a full
-              stack developer.
-              I am a family man, ex truck driver and a vet. I feel motivated to work and create!
+            <p >Full stack web developer and a U.S National Guard vet ready to get out there. Fresh out of the University of Minnesota and earning a certificate in full stack development with newly developed skills in Javascript, CSS, Html, Node.js and Bootstrap responsive designs. Very motivated to solve problems with a focused mindset and ultimately love to be creative with designs. I worked in a team of 3 to develop a DNR app to find information about specific natural areas for outdoor enthusiasts. I make sure to implement friendly UI and UX aspects when developing a design for an app. Ready to learn new things and meet people, as a part of the virtual workforce!
             </p>
 
           </div>
--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const projects = [
   },
   {
     title: "Workday",
-    subtitle: "Node.js, Javascript",
+    subtitle: "Node.js, Javascript, Moment",
     description: "I started with utalizing bootstrap to get my containers with the time, textarea and save button postion I added the hours to the time blocks for an average work schedule from 9 to 5. I moved on the my 'work.js' folder to work on the javascript. I made a clock showing the current time on the top of the page. The text blocks are color coded to the current time. There is a save button to save your text and when you reload the page the text remains.",
     link: "https://github.com/groyseth/Work-Planner-GR",
     live: "https://groyseth.github.io/Work-Planner-GR/"
