61e7caa05a6d508231529301016561e73d025921
Grant Royseth <groyseth@gmail.com>
2022-04-13 23:19:37 -0500

    commit

--- src/data.js
+++ src/data.js
@@ -21,36 +21,36 @@ export const projects = [
     link: "https://github.com/groyseth/Weather-Dash-GR",
     live: "https://groyseth.github.io/Weather-Dash-GR/"
   },
-   {
-    title: "Password",
-    subtitle: "Node.js, Javascript",
-    description: "I utalized JavaScript and a random number generator to make the Password Generator. The password generator creates a randomly generated, password with paramiters of the users choice. You can choose between 8 and 128 characters and if you want lowercase, uppercase, numbers and special characters. ",
-    link: "https://github.com/groyseth/Passwork-Genorator-GR",
-    live: "https://groyseth.github.io/Passwork-Genorator-GR/"
-  },
+  //  {
+  //   title: "Password",
+  //   subtitle: "Node.js, Javascript",
+  //   description: "I utalized JavaScript and a random number generator to make the Password Generator. The password generator creates a randomly generated, password with paramiters of the users choice. You can choose between 8 and 128 characters and if you want lowercase, uppercase, numbers and special characters. ",
+  //   link: "https://github.com/groyseth/Passwork-Genorator-GR",
+  //   live: "https://groyseth.github.io/Passwork-Genorator-GR/"
+  // },
  
-  {
-    title: "E-Commerce",
-    subtitle: "MySQL, Javascript",
-    description: "This e-commerce project you will be able to add, delete and update into pre-existing tables using 'mysql2'. This project is easy to run in insomnia if you follow the video instructions. ",
-    link: "https://github.com/groyseth/E-Commerce-GR",
-    live: "https://watch.screencastify.com/v/jbDFdhhvEYZTDywi538x"
-  },
-  {
-    title: "Employee Tracker",
-    subtitle: "Inquirer, MySQL",
-    description: "In this employee tracker you can add a role, department, and employees! The user input will be saved in the MySQL data base.",
-    link: "https://github.com/groyseth/Employee-Tracker-GR",
-    live: "https://watch.screencastify.com/v/Xjh32kDMmF4Js0QVjNmA"
-  },
+  // {
+  //   title: "E-Commerce",
+  //   subtitle: "MySQL, Javascript",
+  //   description: "This e-commerce project you will be able to add, delete and update into pre-existing tables using 'mysql2'. This project is easy to run in insomnia if you follow the video instructions. ",
+  //   link: "https://github.com/groyseth/E-Commerce-GR",
+  //   live: "https://watch.screencastify.com/v/jbDFdhhvEYZTDywi538x"
+  // },
+  // {
+  //   title: "Employee Tracker",
+  //   subtitle: "Inquirer, MySQL",
+  //   description: "In this employee tracker you can add a role, department, and employees! The user input will be saved in the MySQL data base.",
+  //   link: "https://github.com/groyseth/Employee-Tracker-GR",
+  //   live: "https://watch.screencastify.com/v/Xjh32kDMmF4Js0QVjNmA"
+  // },
  
-  {
-    title: "Piano Project",
-    subtitle: "React",
-    description: "Be able to play the piano with users keyboard or click on the keys themselfs. Still more work I want to do like adding a log in feature and saving songs that are user made.",
-    link: "https://github.com/groyseth/react-piano",
-    live: "https://piano-project-11229.herokuapp.com/"
-  },
+  // {
+  //   title: "Piano Project",
+  //   subtitle: "React",
+  //   description: "Be able to play the piano with users keyboard or click on the keys themselfs. Still more work I want to do like adding a log in feature and saving songs that are user made.",
+  //   link: "https://github.com/groyseth/react-piano",
+  //   live: "https://piano-project-11229.herokuapp.com/"
+  // },
   // {
   //   title: "Tech Blog",
   //   subtitle: "MySQL, Express, Sequilize, Restful API",
