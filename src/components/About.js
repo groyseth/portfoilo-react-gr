aca913ee7a178fc350d3151e4f06547d5c25b7a1
Grant Royseth <groyseth@gmail.com>
2022-05-25 13:55:52 -0500

    hot fix

--- src/components/About.js
+++ src/components/About.js
@@ -23,59 +23,15 @@ export default function About() {
     <section id="about" >
         <Col>
           <div className="container moving">
-            
             <h1>About Me:</h1>
             <p className="wraped">Full Stack Web developer and a U.S National Guard vet ready to get out there. Received a certificate in Full Stack Web Development University of Minnesota with skills in Javascript, CSS, Html, Node.js and Bootstrap responsive designs. Very motivated to solve problems with a focused mindset and ultimately love to be creative with designs. Worked in a team of 3 to develop a Department of Natural Resources app to find information about specific natural areas for outdoor enthusiasts. Made sure to implement friendly UI and UX aspects when developing a design for an app. Ready to learn new things and meet people, as a part of the technical workforce!
             </p>
 
           </div>
         </Col>
-        <Row>
-          <div>
+          <div className="imagePlace">
             <Image  src={pic} alt="Profile" className="profile-pic styleBehind" data-aos="flip-left" ></Image>
 </div>
-
-
-            {/* <Carousel activeIndex={index} onSelect={handleSelect} className="carousel">
-      <Carousel.Item className="carouselItem">
-        <img
-          className="d-block "
-          src={pic}
-          alt="First slide"
-          
-        />
-        
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        
-      </Carousel.Item>
-      
-    </Carousel> */}
-
-          
-      
-        </Row>
-        <Row >
-          <div className="aboutLink " data-aos="zoom-in" >
-     <ul>
-       <h1 className="explore">Explore!</h1>
-     <li className="aboutHover">
-        <a href="#about" className="aboutList" >About Me</a>
-        <a href="#projects" className="aboutList">Projects</a>
-        <a href="#groupProjects" className="aboutList">Group Projects</a>
-        <a href="#contact" className="aboutList">Contact Me</a>
-        
-     </li>
-
-     </ul>
-     </div>
-   </Row>
     </section>
   );
 }
