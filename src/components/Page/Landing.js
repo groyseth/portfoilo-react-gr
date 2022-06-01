3f1ffb784491dadd3dd5205431e67fe11f9fab83
Grant Royseth <groyseth@gmail.com>
2022-05-31 22:12:20 -0500

    commit

--- src/components/Page/Landing.js
+++ src/components/Page/Landing.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import "./Landing.scss"
-
+import pic from "../../assets/textback/noght.png"
 export default function Landing() {
 
   return (
+    <>
     <div className='landingStyle'>
      
       <h1 className='title'>Grant Royseth<br /> Front-End Developer</h1>
      
     </div>
+    <div >
+    <img src={pic} className='astro'></img>
+    </div>
+    </>
   )
 }
